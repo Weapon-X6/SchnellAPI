e0403a8b4867aa8d8f617dfe4a2b8e8b9550b800
waffe_x20 <argueta_eduardo@yahoo.de>
2022-02-23 21:24:59 -0600

    We Shall Remain..**

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -8,6 +9,25 @@ def root():
     return {"We slow down..."}
 
 
-@app.get("/courses/{course_name}")
-def read_course(course_name):
-    return {"course_name": course_name}
+# @app.get("/courses/{course_name}")
+# def read_course(course_name):
+#     return {"course_name": course_name}
+
+
+course_items = [{"course_name": "Python"}, {
+    "course_name": "Ruby"}, {"course_name": "Java"}, ]
+
+
+@app.get("/courses/")
+def read_courses(start: int = 0, end: int = 10):
+    return course_items[start: start + end]
+
+
+course_items = {1: "We", 2: "shall", 3: "remain..."}
+
+
+@app.get("/courses/{course_id}")
+def read_courses(course_id: int, q: Optional[str] = None):
+    if q is not None:
+        return {"course_name": course_items[course_id], "q": q}
+    return {"course_name": course_items[course_id]}
